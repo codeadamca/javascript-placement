ba19d1bd09914318b025c4c76280688592f346b2
Adam <thomasadam83@hotmail.com>
2021-01-12 14:45:04 -0500

    Added sample files

---
+++ external.js
@@ -0,0 +1,8 @@
+
+/*
+This is a JavaScript comment.
+The following lines of JavaScript add an <h1> title and a <p> paragraph to the HTML document.
+*/
+
+document.write( "<h1>External JavaScript</h1>" );
+document.write( "<p>This content comes from an external JavaScript file!</p>" );
